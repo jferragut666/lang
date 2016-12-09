8cd2ceba86140522e614bf17dbf5c42ebeb3326e
Erik Ferragut <erikferragut@gmail.com>
2016-12-09 22:58:39 +0000

    good for me

--- app/views.py
+++ app/views.py
@@ -6,15 +6,32 @@ import flask_wtf
 
 @app.route('/', methods=['post','get'])
 def main():
+    parsedData = 0
+    token = 0
     form = RequestForm()
     if form.validate_on_submit():
+        #parse sentence from the form
         parsedData = app.parser(form.sentence.data)
-        print form.sentence.data
+        sents = []
+        sents = parsedData.sents
+        for span in parsedData.sents:
+            sent = [parsedData[i] for i in range(span.start, span.end)]
+            break
+    lists = []
+    try:    
+        for token in sent:
+            listy=[token.orth_, token.pos_, token.dep_,token.head.orth_]
+            lists.append(listy)
+            print(listy)
+            
+    
+        #print parsedData
         
-    return render_template('base.html', form=form, title = "home")
+        return render_template('base.html', form=form, title = "home", data = lists)
+    except: return ""    
 
 class RequestForm(flask_wtf.FlaskForm):
-    sentence = wtforms.StringField('sentence', [wtforms.validators.InputRequired()])
+    sentence = wtforms.StringField('Sentence', [wtforms.validators.InputRequired()])
     submit = wtforms.SubmitField()
-    
+
     
